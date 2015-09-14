947eaa48dd0bedd8366106a1bf4370ac9d69cdb2
Tuan Le <tuan.le@exe.com.vn>
2015-09-14 11:30:36 +0700

    Get config from env

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
-  'secret' => 'BupsNIvC$YE00495anfYz32&f19km43ymJ83b!ilT6tNgk5hNzFZKV&a$vqOEkdlA5ZfrS252flphjsIyusWbO7GTX%$v6c3rePR',
-  'alg'  => 'HS256',
-  'identify' => 'email',
-  'ttl' => '300' // Second
+  'secret' => env('TOKEN.SECRET', 'BupsNIvC$YE00495anfYz32&f19km43ymJ83b!ilT6tNgk5hNzFZKV&a$vqOEkdlA5ZfrS252flphjsIyusWbO7GTX%$v6c3rePR'),
+  'alg'  => env('TOKEN.ALG', 'HS256'),
+  'identify' => env('TOKEN.IDENTIFY', 'email'),
+  'ttl' => env('TOKEN.TTL', 300) // Second
 ];
